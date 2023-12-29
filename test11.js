a6ccd5152fd0d5f8c5447214383cf505c89cff3c
H. M. SHIHAB <shihab2305341402@diu.edu.bd>
2023-12-29 21:12:04 +0600

    rf

---
+++ test11.js
@@ -0,0 +1,14 @@
+//Program to find the sum using function
+//This is a basic formation of function in JavaScript:
+function myfunction(a, b, c) {
+    return a + b + c;
+}
+console.log("The sum is: ", myfunction(4 , 4, 4,))
+
+//Finding the average of 4 numbers:
+function avg(a,b,c,d) {
+    return (a + b + c + d) / 4;
+}
+console.log("The average of these numbers is: " + avg(6,3,5,7))
+
+// ** One thing to remember "In JS,the division of two numbers may comes floating number.we don't have to convert the type."
