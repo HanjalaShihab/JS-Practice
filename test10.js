072e9e73616eeeb4990839bb900a6f2129c95b4e
H. M. SHIHAB <shihab2305341402@diu.edu.bd>
2023-12-29 14:55:48 +0600

    g

---
+++ test10.js
@@ -0,0 +1,8 @@
+//Write a program to print "Try again" untill the user enters the correct number.
+let cn = 10;
+let i;
+while( i != cn) {
+    console.log("Try again");
+    i = prompt("Enter any number: ");
+}
+console.log("You have entered the correct number");
