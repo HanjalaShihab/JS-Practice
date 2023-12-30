50c10e2b896139898a560d79c5dae40d4b1571d7
H. M. SHIHAB <shihab2305341402@diu.edu.bd>
2023-12-30 15:58:05 +0600

    d

--- test11.js
+++ test11.js
@@ -12,3 +12,23 @@ function avg(a,b,c,d) {
 console.log("The average of these numbers is: " + avg(6,3,5,7))
 
 // ** One thing to remember "In JS,the division of two numbers may comes floating number.we don't have to convert the type."
+
+//Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+let x = prompt("x? ");
+let n = prompt("n? ");
+
+function pow(x, n) {
+    let result = x;
+    for ( let i = 1; i< n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+ if( n < 1 ) {
+    alert(`POwer ${n} is not supported,please enter a positive integer number`);
+ }
+ else {
+    alert(pow(x, n));
+ }
