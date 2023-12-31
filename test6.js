de30790b0045f2d1a1b05ed5f687596992e58a2b
H. M. SHIHAB <shihab2305341402@diu.edu.bd>
2023-12-31 18:29:27 +0600

    updated

--- test6.js
+++ test6.js
@@ -9,3 +9,11 @@ let n2 = ["JavaScript","HTML", "CSS", "REACT"];
 for(let a of n2) {
     console.log(a);
 }
+
+
+//printing the string index by index:
+let name = "JavaScript";
+
+for(let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+}
