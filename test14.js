0cf9c66c0acb76b9fd0a7cfe62e65f7ef786170d
H. M. SHIHAB <shihab2305341402@diu.edu.bd>
2024-01-02 15:49:14 +0600

    Array added

---
+++ test14.js
@@ -0,0 +1,10 @@
+//Array toString()
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let result = numbers.toString();
+
+console.log(result);
+console.log(typeof result); //get the type 
+
+console.log(typeof numbers); //get the type
